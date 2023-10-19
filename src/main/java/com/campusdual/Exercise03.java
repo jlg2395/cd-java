512731d1737bf17ad9fc06dcb6de7a9d1dfa8325
jlg2395 <jorgeseadur@gmail.com>
2023-10-19 10:32:42 +0200

    add exercise 03

---
+++ src/main/java/com/campusdual/Exercise03.java
@@ -0,0 +1,19 @@
+package com.campusdual;
+
+public class Exercise03 {
+    public static  String saludo(String nombre){
+        String mensaje = "Hola. Bienvenido " + nombre;
+        return mensaje;
+    }
+    public static void fechaActual(String fecha){
+
+        System.out.println(fecha);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(saludo("Jorge"));
+        fechaActual("19/03/2021");
+    }
+}
