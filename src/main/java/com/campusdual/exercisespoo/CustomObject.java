fab71e8f1ede1259ceb651877084cce4febc82f3
jlg2395 <jorgeseadur@gmail.com>
2023-11-01 11:35:13 +0100

    Custom object corregida

--- src/main/java/com/campusdual/exercisespoo/CustomObject.java
+++ src/main/java/com/campusdual/exercisespoo/CustomObject.java
@@ -1,40 +1,46 @@
 package com.campusdual.exercisespoo;
 
 public class CustomObject {
-    private int actualFuel=10; // Hemos cambiado el acceso de public a private
-    private static String fuelLabel= "Gasolina"; //cambiada de public a private
+    private int actualFuel = 10; //hemos cambiado de public a private
 
-    public int getActualFuel(){
+    private String fuelLabel = "Gasolina"; //hemos cambiado de public a private. Quitamos static
+
+    //a partir de aquí todos los métodos no estáticos (getters y setters)
+//uso del this
+    public int getActualFuel() {
         return this.actualFuel;
     }
 
-    public void setActualFuel(int actualFuel){
-        if (actualFuel>=0){
-            this.actualFuel= actualFuel;
+    public void setActualFuel(int actualFuel) {
+        if (actualFuel >= 0) {
+            this.actualFuel = actualFuel;
         } else {
             System.out.println("A capacidade non pode ser negativa.");
         }
     }
 
-    public static String getFuelLabel(){
-        return CustomObject.fuelLabel;
-    }
-    public static void setFuelLabel(String fuelLabel){
-        CustomObject.fuelLabel = fuelLabel;
-    }
-    public void showDetails(){
-        System.out.println("O tipo de combustible é: " + CustomObject.getFuelLabel());
+    public String getFuelLabel() {
+        return this.fuelLabel;
     }
 
+    public void setFuelLabel(String fuelLabel) {
+        this.fuelLabel = fuelLabel;
+    }
 
-        public static void main(String[] args) {
-            CustomObject cO = new CustomObject(); //cremos una instancia de la clase CustomObject
-            cO.showDetails(); //accedemos de forma no estática al método del objeto (de la instancia)
-            CustomObject.setFuelLabel("Diésel"); //accedemos de forma estática al método de la clase
-            cO.showDetails();
-            System.out.println("O tipo de combustible da clase é: " + CustomObject.getFuelLabel());
-        }
+    public void showDetails() {
+        System.out.println("O tipo de combustible é: " + this.getFuelLabel());
+    }
 
+    //creamos el main() y con la encapsulación, accedemos de forma no estática
+//primero creamos el objeto y luego accedemos a los métodos
+    public static void main(String[] args) {
+        CustomObjectEncapsulated cO = new CustomObjectEncapsulated();
+        cO.showDetails();
 
+        cO.setFuelLabel("Diésel");
+        cO.showDetails();
+
+        System.out.println("O tipo de combustible da clase é: " + cO.getFuelLabel());
+    }
 }
 
